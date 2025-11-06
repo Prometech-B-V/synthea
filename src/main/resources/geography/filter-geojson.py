006c2b82bfd1273d6a69f94ca3c8de1f2abb7b6f
Fabio Dijkshoorn <offabio@outlook.com>
2025-11-06 11:51:40 +0100

    feat: add script to remove geometry from GeoJSON features
    Introduced a Python script that removes the geometry property from GeoJSON features. This utility supports preprocessing GeoJSON data and simplifies use cases that do not require geometric details.

---
+++ src/main/resources/geography/filter-geojson.py
@@ -0,0 +1,10 @@
+import json
+
+with open("./export.geojson") as f:
+    data = json.load(f)
+    result = dict()
+
+    for feature in data['features']:
+        del feature['geometry']
+
+    json.dump(data, open("./export-fixed.geojson", "w"))
